4240f833ad47fc74b4f3e37af68d1e8aed9c1ab9
Vladimir Dimitrov <dimitrov@vladko.org>
2015-04-07 19:44:16 +0300

    update_weather

--- vali_me/api_sync.php
+++ vali_me/api_sync.php
@@ -1,7 +1,7 @@
 <?php
 include('simple_html_dom.php');
 $link = mysqli_connect("127.0.0.1", "web_course", "asdasd123", "vali_me") or die("Error db"); 
-$query = mysqli_query($link, "SELECT name FROM cities") or die("Error db3"); 
+$query = mysqli_query($link, "SELECT name FROM cities WHERE api_id=0") or die("Error db3"); 
 $cities = array();
 
 while ($row = $query->fetch_object())
@@ -10,12 +10,30 @@ while ($row = $query->fetch_object())
 }
 foreach ($cities as $city) 
 {
-	echo $city;
+	#echo $city.' - ';
 	$url = "http://api.openweathermap.org/data/2.5/weather?q=$city&mode=xml&units=metric";
-	echo $url."<br>";
-/*	$context  = stream_context_create(array('http' => array('header' => 'Accept: application/xml')));
+	#echo $url."<br>";
+	$context  = stream_context_create(array('http' => array('header' => 'Accept: application/xml')));
 	$xml = file_get_contents($url, false, $context);
-	$xml = simplexml_load_string($xml);
-	$arr=$xml->city->attributes();
-	$city_api_id = $arr[0];*/
+	$xml = rtrim($xml, "\n");
+	$xml = rtrim($xml, "\t");
+	$xml = rtrim($xml, " ");
+	$xml = rtrim($xml, "\r");
+	if ($xml == '{"message":"Error: Not found city","cod":"404"}')
+	{
+		mysqli_query($link, "DELETE FROM cities WHERE name=\"$city\"");		
+	}
+	else
+	{
+		$xml = simplexml_load_string($xml);
+
+		if (is_object($xml->city))
+		{
+			$arr=$xml->city->attributes();
+			$city_api_id = $arr[0];
+			mysqli_query($link, "UPDATE cities SET api_id=$city_api_id WHERE name=\"$city\"");
+		}
+		sleep(2);
+	}
 }
+?>
